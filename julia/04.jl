01623dc8c97191e1c834cfebe45d4a09aa77a3d8
Ben Scofield <git@turrean.com>
2015-01-21 19:34:46 -0500

    Stuck on Julia 04

--- julia/04.jl
+++ julia/04.jl
@@ -1,1 +1,16 @@
-println("Not implemented")
+#!/usr/bin/env julia
+
+import Requests
+import JSON
+
+response = Requests.get("https://api.github.com/events")
+
+# stuck; any hit to an HTTPS URL returns
+  # ERROR: stream is closed or unusable
+  #  in check_open at stream.jl:294
+  #  in write at stream.jl:730
+
+#data = JSON.parse(response.data[1:10])
+#println(data)
+
+
--- julia/setup.sh
+++ julia/setup.sh
@@ -2,4 +2,4 @@
 sudo add-apt-repository ppa:staticfloat/juliareleases
 sudo add-apt-repository ppa:staticfloat/julia-deps
 sudo apt-get -y update
-sudo apt-get -y install julia
+sudo apt-get -y install julia libnettle4
